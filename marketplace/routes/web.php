8714d71213acc675c9a23f6ceb35f5438155858d
hamdaouiwassim <hamdaouiwassim@gmail.com>
2026-03-11 15:25:17 +0100

    Lesson 6

--- marketplace/routes/web.php
+++ marketplace/routes/web.php
@@ -30,6 +30,34 @@ Route::get("/contact" , function(){
 
 Route::get("/about" , function(){
 
-    return view("contct");
+    return view("contact");
 }
 );
+
+
+
+Route::get("/data/show" , function(){
+
+    $name = "wassim";
+    $age = 33;
+    $birth_date = 1992;
+
+    $data = [
+        'name'=>"wassim" , 
+        'age'=>33 ,
+        'birth_date'=>1992];
+
+    // 1-Parsing data using with()
+    //return view('sharing_data')->with( 'age' , $age)->with('name' , $name);
+    // 2-Parsing data using compact()
+    //return view('sharing_data',compact('name','age','birth_date'));
+    // 3-Parsing data using an array
+    return view('sharing_data',$data);
+
+    // 4-Sharing data with all views
+    // 5-Using view composer
+
+    // best practice : compact 
+}
+);
+
